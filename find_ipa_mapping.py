afba36dd6c978c30afcded61277a58fbc70836a5
Paul Moore <lapilosew2003@gmail.com>
2019-10-16 16:40:03 +0100

    Started prepping to find IPA mappings

---
+++ find_ipa_mapping.py
@@ -0,0 +1,40 @@
+import os, epitran
+from os.path import join, exists
+import global_vars
+import py_helper_functions as helper
+
+def get_transcript_files(language_code):
+    lang_dir = join(global_vars.wav_dir, language_code)
+    assert exists(lang_dir), lang_dir + " not found!"
+    # First check if there is a "rmn" folder containing romanised transcripts
+    if "rmn" in os.listdir(lang_dir):
+        tr_dir = join(lang_dir, "rmn")
+    # If not, use the "trl" folder which has special characters
+    else:
+        tr_dir = join(lang_dir, "trl")
+    assert exists(tr_dir), tr_dir + " not found!"
+
+    # Find dictionary directory, check exists
+    dict_dir = join(lang_dir, "dict")
+    assert exists(dict_dir), dict_dir + " not found!"
+    # Find dictionary file (may be .dict or .txt)
+    dict_files = [x for x in os.listdir(dict_dir) if x.endswith(".dict") or x.endswith(".txt")]
+    if len(dict_files) > 1:
+        print("WARNING: multiple dictionary files found: " + dict_files)
+    dict_file = join(dict_dir, dict_files[0])
+
+    return helper.listdir_fullpath(tr_dir), dict_file
+
+
+def main():
+    language_code = "HA"
+    tr_files, dict_file = get_transcript_files(language_code)
+    epi_dict = helper.read_epitran_codes()
+    epi_code = epi_dict[language_code]
+    assert epi_code != "None", "Language cannot be used with Epitran"
+    if language_code in ["AR", "FR", "PO"]:
+        print("WARNING: epitran quite inaccurate for " + language_code)
+
+
+if __name__ == "__main__":
+    main()
---
+++ py_helper_functions.py
@@ -0,0 +1,41 @@
+import os
+from os.path import join, exists
+import global_vars
+
+""""
+File containing various python functions which are commonly used
+"""
+def listdir_fullpath(d):
+    return [join(d, f) for f in os.listdir(d)]
+
+# Create 1:1 dict mapping e.g. dict[AR] = Arabic, dict[Arabic] = AR
+def read_lang_codes():
+    lang_dict = {}
+    all_langs = []
+    file_path = join(global_vars.conf_dir, "lang_codes.txt")
+    assert exists(file_path), "Could not find lang_codes.txt under " + global_vars.conf_dir
+    with open(file_path, "r") as f:
+        lines = f.read().splitlines()
+    for line in lines:
+        entry = line.split()
+        lang_code = entry[0]
+        lang_word = entry[1]
+        lang_dict[lang_code] = lang_word
+        lang_dict[lang_word] = lang_code
+        all_langs.append(lang_word)
+    return lang_dict, all_langs
+
+# Get dictionary mapping from lang code to epitran-code
+# e.g. dict[HA] = "hau-Latn"
+def read_epitran_codes():
+    epi_dict = {}
+    file_path = join(global_vars.conf_dir, "epitran_codes.txt")
+    assert exists(file_path), "Could not find epitran_codes.txt under " + global_vars.conf_dir
+    with open(file_path, "r") as f:
+        lines = f.read().splitlines()
+    for line in lines:
+        entry = line.split()
+        lang_code = entry[0]
+        epitran_code = entry[1]
+        epi_dict[lang_code] = epitran_code
+    return epi_dict
